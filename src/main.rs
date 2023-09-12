183efcdb1ab622c4881291d72e40a0279c67e191
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-09-12 16:25:47 +0200

    doc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 //! A first template piece of code for example.
 //!
+//! See [`String`](std::string::String) documentation for more details.
+
+
 // a simple hello world program
 fn main() {
     println!("Hello, world!");
